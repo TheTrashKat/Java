9a0dd88367e09337145df36974729f860b161cfb
Trashkat <onemereday@gmail.com>
2018-12-20 02:25:11 -0500

    En esta Fase ya podemos conectarnos con el servidor, hubo un error con porque al parecer necesitaba darle autentificaciones para acceder por ahora descarga un docmuento pdf

--- FilesDownloader/src/Controller/main.java
+++ FilesDownloader/src/Controller/main.java
@@ -11,19 +11,22 @@ public class main {
 		try {
 			// Url con la foto
 			URL url = new URL(
-					"https://assets.trome.pe/files/ec_article_multimedia_gallery/uploads/2018/04/17/5ad609d27c1a7.jpeg");
+					"http://www.skynovels.net/read-offline/4106/1.pdf");
 
 			// establecemos conexion
 			URLConnection urlCon = url.openConnection();
-
+			urlCon.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+			urlCon.connect();
 			// Sacamos por pantalla el tipo de fichero
-			System.out.println(urlCon.getContentType());
+			//System.out.println(urlCon.getContentType());
 
 			// Se obtiene el inputStream de la foto web y se abre el fichero
 			// local.
 			InputStream is = urlCon.getInputStream();
-			FileOutputStream fos = new FileOutputStream("D:/foto.jpg");
-
+			FileOutputStream fos = new FileOutputStream("D:/prueba1.pdf");
+			
+			
+			
 			// Lectura de la foto de la web y escritura en fichero local
 			byte[] array = new byte[1000]; // buffer temporal de lectura.
 			int leido = is.read(array);
@@ -33,6 +36,7 @@ public class main {
 			}
 
 			// cierre de conexion y fichero.
+			System.out.println("Aricho "+urlCon.getFileNameMap());
 			is.close();
 			fos.close();
 		} catch (Exception e) {
