a8baa27cb6ff97dae0cab5d1f791406e8bd6c387
Trashkat <onemereday@gmail.com>
2018-12-20 02:07:42 -0500

    ??

---
+++ FilesDownloader/src/Controller/main.java
@@ -0,0 +1,45 @@
+package Controller;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class main {
+
+	
+	public static void main(String[] args) {
+		try {
+			// Url con la foto
+			URL url = new URL(
+					"https://assets.trome.pe/files/ec_article_multimedia_gallery/uploads/2018/04/17/5ad609d27c1a7.jpeg");
+
+			// establecemos conexion
+			URLConnection urlCon = url.openConnection();
+
+			// Sacamos por pantalla el tipo de fichero
+			System.out.println(urlCon.getContentType());
+
+			// Se obtiene el inputStream de la foto web y se abre el fichero
+			// local.
+			InputStream is = urlCon.getInputStream();
+			FileOutputStream fos = new FileOutputStream("D:/foto.jpg");
+
+			// Lectura de la foto de la web y escritura en fichero local
+			byte[] array = new byte[1000]; // buffer temporal de lectura.
+			int leido = is.read(array);
+			while (leido > 0) {
+				fos.write(array, 0, leido);
+				leido = is.read(array);
+			}
+
+			// cierre de conexion y fichero.
+			is.close();
+			fos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+}
---
+++ ProyectoFinal/bin/lol.sql
@@ -0,0 +1,29 @@
+--
+-- File generated with SQLiteStudio v3.2.1 on mar. nov. 13 19:06:13 2018
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: Jugador
+CREATE TABLE Jugador (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nombre STRING, infuence INT);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (10, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (11, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (12, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (13, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (14, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (15, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (16, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (17, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (18, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (19, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (20, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (21, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (22, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (23, 'Finished Goods', 5000);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (24, 'pepe', 1);
+INSERT INTO Jugador (id, nombre, infuence) VALUES (25, 'diego', 1);
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
